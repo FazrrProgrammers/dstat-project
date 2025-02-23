0f28957f4d713252f96adbcda01aed8bd17a817d
FazrrProgrammers <ibung512@gmail.com>
2025-02-23 19:09:17 +0800

    Update requests.js

--- api/requests.js
+++ api/requests.js
@@ -1,7 +1,12 @@
-export default function handler(req, res) {
-    if (!globalThis.requestCount) {
-        globalThis.requestCount = 0; // Inisialisasi jika belum ada
+let requestCount = 0;
+
+setInterval(() => {
+    if (requestCount > 0) {
+        requestCount = Math.floor(requestCount * 0.8); // Kurangi 20% setiap menit
     }
-    globalThis.requestCount++; // Tambah jumlah request
-    res.status(200).json({ requestCount: globalThis.requestCount });
+}, 60000); // 60 detik
+
+export default function handler(req, res) {
+    requestCount++;
+    res.status(200).json({ requestCount });
 }
